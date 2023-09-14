5bf7d45bb00f225be2841348af119d162ac0308e
NatalyHofkamp <nhofkamp@udesa.edu.ar>
2023-09-13 23:38:36 -0300

    diente de sierra

--- funciones.py
+++ funciones.py
@@ -1,52 +1,61 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(x,y,title,xlabel,ylabel,legend):
+def plot(x, y, title, xlabel, ylabel, legend):
     plt.figure()
-    plt.stem(x,y)
+    plt.stem(x, y, label=legend, linefmt='b-', markerfmt='bo', basefmt=' ')
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.tight_layout()
-    # plt.legend(legend)
+    plt.legend([legend])
     plt.show()
 
-# Generar una señal diente de sierra
-def generar_diente_de_sierra(pendiente, periodo, tiempo_total):
-    tiempo = np.linspace(0, tiempo_total, int(100))
-    señal = pendiente * (tiempo % periodo)
-    plot(tiempo,señal,'Señal Diente de Sierra','tiempo','amplitud','diente de sierra')
+def diente_de_sierra(A, f, muestras):
+    señal = A * (muestras * f - np.floor((1/2) + f * muestras))
+    plot(muestras, señal, 'Señal Diente de Sierra', 'Tiempo', 'Amplitud', 'Diente de Sierra')
 
-
-def tren_de_pulsos(A,w,tiempo_total,muestras):
-    # Generar la señal
-    tiempo = np.linspace(0, tiempo_total,muestras)  # Crear un vector de tiempo
-    señal = A * np.sign(np.sin(w* tiempo))  # Calcular la señal
-    plot(tiempo,señal,'Señal x(t)','Tiempo (s)','Amplitud','tren de pulsos')
-   
-def serie_tren_de_pulsos(a0,bn,w,muestras,cant_armonicos):
+def serie_diente_de_sierra(A, a0, w, muestras, cant_armonicos, umbral=1e-6):
     serie = []
     for t in muestras:
         armonicos = 0
         for n in range(1, cant_armonicos + 1):
-            armonicos+=((bn / n) * np.sin(n * w * t))
-        serie.append((a0 / 2) + armonicos)
-    plot(muestras,serie,'serie del tren de pulsos',"muestras",'serie','jkfwoj')
+            armonico = ((-1) ** (n + 1)) * (A / (np.pi * n)) * np.sin(w * n * t)
+            if abs(armonico) < umbral:
+                armonico = 0  
+            armonicos += armonico
+            #depende por cuánto divido el a0,se grafican mejor o peor la parte de abajo
+        serie.append((a0/2) + armonicos)
+    plot(muestras, serie, 'Serie Diente de Sierra', 'Muestras', 'Serie', 'serie diente de sierra')
+
+def tren_de_pulsos(A,w, tiempo_total, muestras):
+    tiempo = np.linspace(0, tiempo_total, muestras)
+    señal = np.sign(np.sin(w * tiempo))
+    plot(tiempo, señal, 'Señal x(t)', 'Tiempo (s)', 'Amplitud', 'Tren de Pulsos')
+
+def serie_tren_de_pulsos(w, muestras, cant_armonicos):
+    serie = []
+    for t in muestras:
+        armonicos = 0
+        for n in range(1, cant_armonicos + 1):
+            armonicos += ((2 / (np.pi * w) / n) * np.sin(n * w * t))
+        serie.append(armonicos)
+    plot(muestras, serie, 'Serie del Tren de Pulsos', 'Muestras', 'Serie', 'serie tren de pulsos')
 
 
 def main():
     tiempo_total = 2.0
-    A = 1.0       # Amplitud
-    T = (2*np.pi)    # Frecuencia en Hz
-    tiempo_total = 6*np.pi # Duración de la señal en segundos
-    w= (2*np.pi)/T
-    muestras =  int(100)
-    pendiente_diente_de_sierra = 1.0
-    periodo_diente_de_sierra = 1.0
-    generar_diente_de_sierra(pendiente_diente_de_sierra, periodo_diente_de_sierra, tiempo_total)
-    tren_de_pulsos(A,w,tiempo_total,muestras)
+    A = 1.0      
+    T = (2*np.pi)   
+    tiempo_total = 6*np.pi 
+    w= (2*np.pi)/T 
     muestras = np.linspace(0,2*np.pi,100)
-    serie_tren_de_pulsos(0,(A*2)/(np.pi*w),w,muestras,10)
+    muestras_diente = np.linspace(0,20,100)
+    diente_de_sierra(A,1/2,muestras_diente)
+    serie_diente_de_sierra(A,A/2,np.pi,muestras_diente,30)
+    tren_de_pulsos(A,w,tiempo_total,100)
+    serie_tren_de_pulsos(w,muestras,10)
    
 if __name__ =='__main__':
     main()
